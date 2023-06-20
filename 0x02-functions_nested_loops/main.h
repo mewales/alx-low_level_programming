6d0f7d48cf3c97eac9c7d26e0f60aa37c54f9576
mewales <akinlade539@yahoo.com>
2023-06-20 14:21:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,9 +9,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-oid times_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int_putchar(char c);
+int _putchar(char c);
 
 #endif
